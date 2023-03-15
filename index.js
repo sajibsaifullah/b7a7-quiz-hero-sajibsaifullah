016cae58e7965ccef75fe3105e77cca76bae6ba0
sajibsaifullah <dusajibphy@gmail.com>
2023-03-15 23:33:25 +0600

    blog linked

--- index.js
+++ index.js
@@ -182,3 +182,7 @@ document.querySelector("#submit").addEventListener("click", () => {
   }, 1500);
   window.scrollTo(0, 0);
 });
+
+const openBlog = () => {
+  window.location.href = 'blog.html'
+}
